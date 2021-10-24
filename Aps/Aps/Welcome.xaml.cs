922869f6531688c63f81dabb6d89ade1d2284c1e
saivamshii <84490491+saivamshii@users.noreply.github.com>
2021-10-24 17:19:13 +0530

    Add project files.

---
+++ Aps/Aps/Welcome.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aps
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Welcome : ContentPage
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new Menu());
+        }
+    }
+}
